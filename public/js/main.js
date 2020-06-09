cf6ec44b608670dd8623e38456006f22d06d1cdc
Dani Ihza Farrosi <dani.videda@gmail.com>
2020-06-09 15:16:04 +0700

    Navbar change on scroll, add button class

--- public/js/main.js
+++ public/js/main.js
@@ -2,3 +2,11 @@
 //   e.preventDefault();
 //   $(this).tab('show');
 // });
+
+window.addEventListener('scroll', function () {
+  let header = document.querySelector('nav');
+  // let items = document.querySelectorAll('.items')
+  let windowPosition = window.scrollY > 0;
+  header.classList.toggle('nav-scrolling-active', windowPosition);
+  // items.classList.toggle('items-scrolling-active', windowPosition);
+});
